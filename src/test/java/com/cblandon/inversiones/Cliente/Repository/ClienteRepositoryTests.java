13c9c57efd2786b1bb99d0e0078a1bcfb4bbad13
Cristian Blandon <cblandon@playtechla.com>
2023-10-03 09:02:39 -0500

    se realiza test a  JPA entidad de cliente
    test

---
+++ src/test/java/com/cblandon/inversiones/Cliente/Repository/ClienteRepositoryTests.java
@@ -0,0 +1,81 @@
+package com.cblandon.inversiones.Cliente.Repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.cblandon.inversiones.Cliente.Cliente;
+import com.cblandon.inversiones.Cliente.ClienteRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE )
+public class ClienteRepositoryTests {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    private Cliente cliente;
+
+    @BeforeEach
+    void setup() {
+        cliente = Cliente.builder()
+                .nombres("Christian")
+                .apellidos("Ramirez")
+                .email("c1@gmail.com")
+                .celular("3104657100")
+                .pais("colombia")
+                .cedula("11414")
+                .build();
+    }
+
+    @DisplayName("Test para guardar un cliente")
+    @Test
+    void testGuardarCliente() {
+        //given - dado o condición previa o configuración
+        Cliente cliente2 = Cliente.builder()
+                .nombres("Christian")
+                .apellidos("Ramirez")
+                .email("ce@gmail.com")
+                .celular("3104657100")
+                .pais("colombia")
+                .cedula("1141400")
+                .build();
+
+        //when - acción o el comportamiento que vamos a probar
+        Cliente clienteGuardado = clienteRepository.save(cliente2);
+
+        //then - verificar la salida
+        assertThat(clienteGuardado).isNotNull();
+        assertThat(clienteGuardado.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("Test para listar todos los clientes")
+    @Test
+    void testListarClientes(){
+        //given
+        Cliente cliente2 = Cliente.builder()
+                .nombres("Christian")
+                .apellidos("Ramirez")
+                .email("ce@gmail.com")
+                .celular("3104657100")
+                .pais("colombia")
+                .cedula("1141400")
+                .build();
+        clienteRepository.save(cliente2);
+        clienteRepository.save(cliente);
+
+        //when
+        List<Cliente> listaClientes = clienteRepository.findAll();
+
+        //then
+        assertThat(listaClientes).isNotNull();
+    }
+
+}
