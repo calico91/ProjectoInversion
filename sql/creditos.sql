b81c5bf718417c87cdd08a87c9f497ad0df2e664
Cristian Blandon <cblandon@playtechla.com>
2023-11-03 09:16:21 -0500

    actualizacion bd

--- sql/creditos.sql
+++ sql/creditos.sql
@@ -2,7 +2,7 @@
 -- Query: SELECT * FROM apirest.credito
 LIMIT 0, 1000
 
--- Date: 2023-11-01 12:45
+-- Date: 2023-11-02 16:00
 */
 INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (2,'A','2023-10-24','maelito',1);
 INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (3,'A','2023-10-09','maelito',2);
@@ -18,3 +18,7 @@ INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_c
 INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (25,'A','2023-09-01','maelito',13);
 INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (26,'A','2023-08-01','maelito',26);
 INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (27,'A','2023-07-01','maelito',25);
+INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (28,'A','2023-06-30','maelito',34);
+INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (30,'A','2023-10-02','blandon',7);
+INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (32,'A','2023-05-01','blandon',36);
+INSERT INTO `` (`id_credito`,`estado_credito`,`fecha_credito`,`usuario_creador_credito`,`id_cliente`) VALUES (33,'A','2023-08-01','blandon',23);
