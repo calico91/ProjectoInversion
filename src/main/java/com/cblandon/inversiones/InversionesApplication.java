0519e5e56b5f30f59a9ce34308ac698bc29dc7dd
Cristian Andres <103708283+calico91@users.noreply.github.com>
2023-12-04 08:47:35 -0500

    cambiar zona horaria

--- src/main/java/com/cblandon/inversiones/InversionesApplication.java
+++ src/main/java/com/cblandon/inversiones/InversionesApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 public class InversionesApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT-5:00"));
         SpringApplication.run(InversionesApplication.class, args);
     }
 
