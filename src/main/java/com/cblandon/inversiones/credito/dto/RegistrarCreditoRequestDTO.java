3831d53cae78e4d8626a5a09de2a0da9d68a541d
Cristian Andres <103708283+calico91@users.noreply.github.com>
2024-01-26 14:53:55 -0500

    se realiza cambio, se elimina de la tabla cuota credito el valor del credito y se adicciona el campo saldo credito

--- src/main/java/com/cblandon/inversiones/credito/dto/RegistrarCreditoRequestDTO.java
+++ src/main/java/com/cblandon/inversiones/credito/dto/RegistrarCreditoRequestDTO.java
@@ -1,6 +1,10 @@
 package com.cblandon.inversiones.credito.dto;
 
 
+import com.cblandon.inversiones.utils.Constantes;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -11,10 +15,15 @@ import java.time.LocalDate;
 @Builder
 
 public class RegistrarCreditoRequestDTO {
+
+    @NotNull(message = Constantes.NOT_NULL)
     private Double interesPorcentaje;
 
+    @NotNull(message = Constantes.NOT_NULL)
     private Integer cantidadCuotas;
 
+    @NotNull(message = Constantes.NOT_NULL)
+    @NotBlank(message = Constantes.NOT_BLANK)
     private String cedulaTitularCredito;
 
     //@JsonFormat(pattern = "dd/MM/yyyy")
@@ -23,8 +32,11 @@ public class RegistrarCreditoRequestDTO {
     //@JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate fechaCredito;
 
+    @NotNull(message = Constantes.NOT_NULL)
     private Double valorCredito;
 
+    @NotNull(message = Constantes.NOT_NULL)
+    @NotBlank(message = Constantes.NOT_BLANK)
     private String modalidad;
 
 }
