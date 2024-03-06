68c55a897d40a2b54bb0b2ba4091f2e7017bf8b6
Cristian Andres <103708283+calico91@users.noreply.github.com>
2024-03-06 08:14:34 -0500

    se realiza cambios a mapper

--- src/main/java/com/cblandon/inversiones/mapper/CuotaCreditoMapper.java
+++ src/main/java/com/cblandon/inversiones/mapper/CuotaCreditoMapper.java
@@ -4,9 +4,10 @@ import com.cblandon.inversiones.cuotacredito.CuotaCredito;
 import com.cblandon.inversiones.cuotacredito.dto.AbonosRealizadosResponseDTO;
 import com.cblandon.inversiones.cuotacredito.dto.CuotasCreditoResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.factory.Mappers;
 
-@Mapper()
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CuotaCreditoMapper {
 
     CuotaCreditoMapper mapperCuotaCredito = Mappers.getMapper(CuotaCreditoMapper.class);
--- src/main/java/com/cblandon/inversiones/utils/UtilsMetodos.java
+++ src/main/java/com/cblandon/inversiones/utils/UtilsMetodos.java
@@ -1,10 +1,6 @@
 package com.cblandon.inversiones.utils;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 public class UtilsMetodos {
