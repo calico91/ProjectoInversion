f0e962470503044ddb28b0622894ac7e558502fc
Cristian Andres <103708283+calico91@users.noreply.github.com>
2024-03-11 08:43:25 -0500

    se pone builder en clientescuotacredito

--- src/main/java/com/cblandon/inversiones/cliente/dto/ClientesCuotaCreditoDTO.java
+++ src/main/java/com/cblandon/inversiones/cliente/dto/ClientesCuotaCreditoDTO.java
@@ -1,8 +1,11 @@
 package com.cblandon.inversiones.cliente.dto;
 
+import lombok.Builder;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Builder
 public record ClientesCuotaCreditoDTO(
         Integer idCliente, String nombres, String apellidos, String cedula, LocalDate fechaCredito, Double valorCredito,
         LocalDateTime fechaAbono, LocalDate fechaCuota, Double valorCuota, Double valorInteres, Integer idCredito) {
