dd4e7fb3581b13e5307d6faaa9979a74c6f6e554
Cristian Andres <103708283+calico91@users.noreply.github.com>
2024-06-12 09:44:52 -0500

    se controla error de valores nulos

--- src/main/java/com/cblandon/inversiones/reporte/dto/ReporteInteresCapital.java
+++ src/main/java/com/cblandon/inversiones/reporte/dto/ReporteInteresCapital.java
@@ -1,4 +1,9 @@
 package com.cblandon.inversiones.reporte.dto;
 
-public record ReporteInteresCapital(double valorCapital, double valorInteres) {
+public record ReporteInteresCapital(Double valorCapital, Double valorInteres) {
+
+    public ReporteInteresCapital {
+        valorCapital = valorCapital == null ? 0 : valorCapital;
+        valorInteres = valorInteres == null ? 0 : valorInteres;
+    }
 }
