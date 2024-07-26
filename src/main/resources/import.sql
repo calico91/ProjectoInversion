228410a3c5259c0fe66ff9f88b96531c87c84af4
Cristian Andres <barcandres91@gmail.com>
2024-07-26 11:49:13 -0500

    se crean las modalidades iniciales

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,8 @@ INSERT INTO roles (name) VALUES ('SUPER'),('ADMIN'),('COBRADOR'),('GENERICO');
 INSERT INTO user (country,email,firstname,lastname,password,username) VALUES ('Colombia','super@gmail.com','Super','Super','$2a$10$b4ffcTyaDA6jcq4ihwF23.vHW1UuXsvOlkVlfXouYjq19zXUKr/6W','super');
 -- asignar rol a super usuario
 INSERT INTO user_roles (user_id,role_id) VALUES (1,1);
+-- se crea modalidades
+INSERT INTO modalidad (description) VALUES ("mensual"),("quincenal");
 
 
 
